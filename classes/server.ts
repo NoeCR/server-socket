69941db7665bb69dd48d2f6e5a400e3f90970373
NoeCR <noecruzrodriguez@gmail.com>
2019-03-05 22:43:12 +0100

    seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,18 +1,43 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
-
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket from '../sockets/sockets';
 
 export default class Server {
+    private static _instance: Server;
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
-    constructor(){
+    private constructor(){
         this.app = express();
         this.port = SERVER_PORT;
+        // Configuración servidor para funcionar con socket.io
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
+    }
+    // Patrón Singleton
+    public static get instance(){
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets(){
+        console.log('Escuchando conexones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente Conectado', socket.eventDataTime());
+            // Desconectar
+            socket.desconectar(cliente);
+            // Escuchar mensajes
+            socket.mensaje(cliente, this.io);
+        });
     }
 
     start(callback: Function){
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+// Única instancia del servidor
+const server = Server.instance;
 
 // BodyParser: todo los datos enviados son convertidos en objetos de JavaScript 
 server.app.use(bodyParser.urlencoded({extended: true}));
